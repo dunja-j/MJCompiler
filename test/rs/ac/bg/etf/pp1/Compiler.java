96bd44929effdf0065a221fd22c1c2a836d30339
dunja-j <dunjajankovic12@gmail.com>
2026-09-01 12:07:50 +0200

    tested level B and it works

--- test/rs/ac/bg/etf/pp1/Compiler.java
+++ test/rs/ac/bg/etf/pp1/Compiler.java
@@ -24,7 +24,7 @@ import rs.etf.pp1.symboltable.concepts.Struct;
 public class Compiler {
 
 	// Configure the MJ program to compile here (matches test/<PROGRAM_NAME>.mj).
-	private static final String PROGRAM_NAME = "test301_jul";
+	private static final String PROGRAM_NAME = "test302_jul";
 
 	static {
 		DOMConfigurator.configure(Log4JUtils.instance().findLoggerConfigFile());
